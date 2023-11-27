c62c334cc84fdabebc237a02bb3534a08be74eef
dylanwal <dylanwal@mit.edu>
2023-11-26 22:38:30 -0500

    clean up

--- chem_analysis/processing/translations.py
+++ chem_analysis/processing/translations.py
@@ -3,6 +3,7 @@ import abc
 import numpy as np
 
 from chem_analysis.processing.base import ProcessingMethod
+from chem_analysis.utils.general_math import get_slice
 
 
 class Translations(ProcessingMethod, abc.ABC):
@@ -23,22 +24,32 @@ class Horizontal(Translations):
 
 
 class Subtract(Translations):
-    def __init__(self, y_subtract: np.ndarray):
+    def __init__(self, y_subtract: np.ndarray, x_subtract: np.ndarray = None):
         self.y_subtract = y_subtract
+        self.x_subtract = x_subtract  #TODO: add
 
     def run(self, x: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         return x, y - self.y_subtract
 
 
 class AlignMax(Translations):
-    def __init__(self, range_: tuple[int, int] = None, range_index: tuple[int, int] = None):
+    def __init__(self, range_: tuple[int, int] = None, range_index: slice = None):
         self.range_ = range_
         self.range_index = range_index
         self.max_ = None
 
-    def align_max(self, x: np.ndarray, y: np.ndarray):
-        max_indices = np.argmax(data, axis=1)
-        roll_amount = -max_indices + max_indices[0]
-        for i in range(1, data.shape[0]):
-            data[i] = np.roll(data[i], roll_amount[i])
-        return data
+    def run(self, x: np.ndarray, y: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        if self.range_ is not None:
+            slice_ = get_slice(x, self.range_[0], self.range_[1])
+            y_ = y[slice_]
+        elif self.range_index is not None:
+            y_ = y[self.range_index]
+        else:
+            y_ = y
+
+        max_indices = np.argmax(y_)
+        roll_amount = max_indices[0]
+
+        x = np.roll(x, roll_amount)
+        y = np.roll(y, roll_amount)
+        return x, y
